c5d260dcf7c5c67816e3969f5ff6fc82c7d463cd
Ryan Hallock <89486399+ryanhallock@users.noreply.github.com>
2026-04-23 12:26:30 -0500

    fix: import ordering

--- backend/src/main/java/edu/utdallas/cs3354/whatt/service/FlashcardSetService.java
+++ backend/src/main/java/edu/utdallas/cs3354/whatt/service/FlashcardSetService.java
@@ -1,10 +1,10 @@
 package edu.utdallas.cs3354.whatt.service;
 
 import edu.utdallas.cs3354.whatt.dto.request.FlashcardRequest;
-import edu.utdallas.cs3354.whatt.dto.response.FlashcardResponse;
 import edu.utdallas.cs3354.whatt.dto.request.FlashcardSetCreateRequest;
-import edu.utdallas.cs3354.whatt.dto.response.FlashcardSetResponse;
 import edu.utdallas.cs3354.whatt.dto.request.FlashcardSetUpdateRequest;
+import edu.utdallas.cs3354.whatt.dto.response.FlashcardResponse;
+import edu.utdallas.cs3354.whatt.dto.response.FlashcardSetResponse;
 import edu.utdallas.cs3354.whatt.entity.Flashcard;
 import edu.utdallas.cs3354.whatt.entity.FlashcardSet;
 import edu.utdallas.cs3354.whatt.entity.User;
